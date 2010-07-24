d056c673a928b45a01b2cdaab3d2022d4bd314c8
tplate <tplate@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-24 01:02:01 +0000

    added track.info() function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/trackobjs@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/track.info.R
@@ -0,0 +1,22 @@
+track.info <- function(pos=1, envir=as.environment(pos), all=TRUE) {
+    if (all) {
+        envirs <- search()
+        i <- sapply(envirs, function(envir) env.is.tracked(envir=as.environment(envir)))
+        env.list <- lapply(as.list(envirs)[i], as.environment)
+        names(env.list) <- envirs[i]
+        res <- data.frame(row.names=NULL, env.name=envirs[i], pos=seq(len=length(envirs))[i])
+    } else {
+        if (!is.environment(envir))
+            envir <- as.environment(envir)
+        if (missing(pos))
+            if (environmentName(envir)=="R_GlobalEnv")
+                pos <- 1
+            else
+                pos <- match(environmentName(envir), search())
+        res <- data.frame(row.names=NULL, env.name=environmentName(envir), pos=pos)
+        env.list <- structure(list(envir), names=environmentName(envir))
+    }
+    return(cbind(res, data.frame(row.names=NULL, do.call("rbind", lapply(env.list,
+           function(e) c(unlist(track.options(envir=e, c("readonly", "cache"))),
+                         dir=track.dir(envir=e)))))))
+}
