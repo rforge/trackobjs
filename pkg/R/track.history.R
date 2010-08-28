2fcbc8285476a9c5204ae3fa71a1efaa9b93555d
tplate <tplate@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-28 03:27:36 +0000

    make session start message an argument of track.history.start()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/trackobjs@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/track.history.R
+++ pkg/R/track.history.R
@@ -1,4 +1,4 @@
-track.history.start <- function(file=NULL, width=NULL, style=NULL, times=NULL) {
+track.history.start <- function(file=NULL, width=NULL, style=NULL, times=NULL, message="Session start") {
     if (!is.null(file))
         options(incr.hist.file=file)
     if (!is.null(width))
@@ -22,7 +22,7 @@ track.history.start <- function(file=NULL, width=NULL, style=NULL, times=NULL) {
     if (is.null(times) || nchar(times)==0)
         times <- TRUE
     times <- as.logical(times)
-    cat("##------* Starting history at", date(), "*------##\n", file=file, append=TRUE)
+    cat("##------*", message, "at", date(), "*------##\n", file=file, append=TRUE)
     invisible(res)
 }
 track.history.stop <- function() {
