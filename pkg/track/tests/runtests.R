ab51b20fb796abba5fefba27cc61f3cb819e484d
tplate <tplate@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-24 00:21:03 +0000

    make full time-consuming tests not run on CRAN
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/trackobjs@208 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/track/tests/runtests.R
+++ pkg/track/tests/runtests.R
@@ -1,2 +1,7 @@
-library(scriptests)
-runScripTests()
+if (Sys.getenv('_R_CHECK_TIMINGS_')!='') {
+    cat('Not running tests because appear to running on CRAN.\n')
+} else if (require(scriptests)) {
+    runScripTests()
+} else {
+    cat("Not running tests because cannot find 'scriptests' package.\n")
+}
