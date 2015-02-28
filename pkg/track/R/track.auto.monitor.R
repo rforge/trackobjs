27aab554166b33252524b451bf30358ecc7985cc
tplate <tplate@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-28 08:49:17 +0000

    try to make comment more clear
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/trackobjs@246 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/track/R/track.auto.monitor.R
+++ pkg/track/R/track.auto.monitor.R
@@ -1,10 +1,12 @@
 track.auto.monitor <- function(expr, value, ok, visible) {
-    ## Monitors existence of task callbacks for track.sync and re-adds them if
-    ## they have gone missing (task callbacks can go missing if they stop with
-    ## an error, which is what happens when the user interrupts the R process,
-    ## or if there are transient file system problems.)
-    ## Try to be fast -- only look at environments on the search path that
-    ## are candidates for tracking (i.e., not packages).
+    ## A callback that monitors existence of task callbacks
+    ## for track.sync and re-adds them if they have gone
+    ## missing (task callbacks can go missing if they stop
+    ## with an error, which is what happens when the user
+    ## interrupts the R process, or if there are transient
+    ## file system problems.)  Try to be fast -- only look
+    ## at environments on the search path that are
+    ## candidates for tracking (i.e., not packages).
     trace <- getOption("track.callbacks.trace", FALSE)
     if (trace==1) {
         cat("track.auto.monitor: entered at ", date(), "\n", sep="")
